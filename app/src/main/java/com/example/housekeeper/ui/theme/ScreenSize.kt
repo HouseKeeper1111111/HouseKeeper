f334892bac6b3fd0d3e0beeeb5f753c64890b283
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-16 15:39:41 +0300

    feat: Created class for the screen sizes initialization

---
+++ app/src/main/java/com/example/housekeeper/ui/theme/ScreenSize.kt
@@ -0,0 +1,36 @@
+package com.example.housekeeper.ui.theme
+
+import android.content.Context
+import android.util.DisplayMetrics
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class ScreenSize(
+    val height: Dp,
+    val width: Dp
+)
+
+class ScreenSizeProvider(context: Context) {
+    private val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+
+    val height: Dp = (displayMetrics.heightPixels/ displayMetrics.density).dp
+    val width: Dp = (displayMetrics.widthPixels / displayMetrics.density).dp
+}
+
+@Composable
+fun rememberScreenSize(): ScreenSize {
+    val configuration = LocalConfiguration.current
+    val context = LocalContext.current
+
+    return remember(configuration) {
+        val provider = ScreenSizeProvider(context)
+        ScreenSize(
+            height = provider.height,
+            width = provider.width
+        )
+    }
+}

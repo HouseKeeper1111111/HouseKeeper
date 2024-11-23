937b2fdb811183e20dd279f6c94663c9f3824dc9
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 17:30:02 +0300

    feat: Added navigation

--- app/src/main/java/com/example/housekeeper/MainActivity.kt
+++ app/src/main/java/com/example/housekeeper/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.housekeeper.ui.pages.ReminderScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.housekeeper.domain.navigation.NavigationGraph
 import com.example.housekeeper.ui.theme.HouseKeeperTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +13,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navigationController = rememberNavController()
+
             HouseKeeperTheme {
-                ReminderScreen()
+                NavigationGraph(navController = navigationController)
             }
         }
     }

11ee3cea8905c3d121b18b09df28818f2c57a9d1
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 22:02:02 +0300

    feat: Added LazyColumn without category

--- app/src/main/java/com/example/housekeeper/ui/pages/ReminderView.kt
+++ app/src/main/java/com/example/housekeeper/ui/pages/ReminderView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,9 +13,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -47,11 +50,11 @@ fun ReminderView(
         Pair(DrawableConstants.BEDROOM, "Bedroom"),
         Pair(DrawableConstants.LIVING_ROOM, "Living room"),
         Pair(DrawableConstants.PETS, "Pets"),
-        Pair(null, "+"),
+        Pair(null, "+")
     )
-    val withoutCategory = listOf(
-        listOf(DrawableConstants.RAG_ICON, "Clean mirrors", "2d 17h"),
-        listOf(DrawableConstants.BROOM_ICON, "Clean floors", "3d 12h")
+    val withoutCategoryList = listOf(
+        Triple(DrawableConstants.RAG_ICON, "Clean mirrors", "2d 17h"),
+        Triple(DrawableConstants.BROOM_ICON, "Clean floors", "3d 12h")
     )
 
     Scaffold(
@@ -62,9 +65,10 @@ fun ReminderView(
                     .background(color = PinkChampagne)
                     .padding(paddingValues)
                     .padding(
-                        top = (screenSize.height.value * 0.05).dp,
-                        start = (screenSize.width.value * 0.015).dp,
-                        end = (screenSize.width.value * 0.015).dp,
+                        top = (screenSize.height.value * 0.04).dp,
+                        start = (screenSize.width.value * 0.04).dp,
+                        end = (screenSize.width.value * 0.04).dp,
+                        bottom = (screenSize.height.value * 0.04).dp
                     )
             ) {
                 Column(
@@ -80,19 +84,21 @@ fun ReminderView(
                         columns = GridCells.Fixed(2),
                         modifier = Modifier
                             .fillMaxWidth()
-                            .wrapContentHeight(),
-                        verticalArrangement = Arrangement.spacedBy(15.dp)
+                            .wrapContentHeight()
+                            .height((screenSize.height.value * 0.36).dp),
+                        verticalArrangement = Arrangement.spacedBy(15.dp),
+                        horizontalArrangement = Arrangement.spacedBy(15.dp)
                     ) {
                         items(categoriesList) {
                             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                                 Box(
                                     modifier = Modifier
                                         .size(
-                                            width = (screenSize.width.value * 0.45).dp,
+                                            width = (screenSize.width.value * 0.44).dp,
                                             height = (screenSize.height.value * 0.1).dp
                                         )
                                         .clip(shape = RoundedCornerShape(10))
-                                        .background(color = Color.White),
+                                        .background(color = Color.White.copy(alpha = 0.5f)),
                                     contentAlignment = Alignment.Center
                                 ) {
                                     if(it.first != null)
@@ -110,13 +116,44 @@ fun ReminderView(
                             }
                         }
                     }
-                    Spacer(modifier = Modifier.height((screenSize.height.value * 0.05).dp))
+                    Spacer(modifier = Modifier.height((screenSize.height.value * 0.04).dp))
                     Text(
                         text = "Without category",
                         style = HouseKeeperTextStyles.CursedBlack35Black
                     )
                     Spacer(modifier = Modifier.height((screenSize.height.value * 0.03).dp))
-
+                    LazyColumn(verticalArrangement = Arrangement.spacedBy(15.dp),) {
+                        items(withoutCategoryList) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height((screenSize.height.value * 0.1).dp)
+                                    .clip(RoundedCornerShape(10.dp))
+                                    .background(color = Color.White.copy(alpha = 0.5f))
+                                    .padding(20.dp),
+                                contentAlignment = Alignment.CenterStart
+                            ) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Image(
+                                        painter = painterResource(id = it.first),
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .size(38.dp)
+                                    )
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    Text(
+                                        text = it.second,
+                                        style = HouseKeeperTextStyles.EliteTeal22Medium
+                                    )
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    Text(
+                                        text = it.third,
+                                        style = HouseKeeperTextStyles.EliteTeal22Medium
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
             }
         },

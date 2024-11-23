f015cb82d38a5e4f139d92cd47f1ecd16db7d7ed
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 17:06:57 +0300

    feat: Added EliteTeal

--- app/src/main/java/com/example/housekeeper/ui/theme/Color.kt
+++ app/src/main/java/com/example/housekeeper/ui/theme/Color.kt
@@ -10,5 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val WhiteLilac = Color(0xFFE7DFED)
-val SmokyBlack = Color(0xFF121212)
+val PinkChampagne = Color(0xFFE7DFED)
+val CursedBlack = Color(0xFF121212)
+val EliteTeal = Color(0xFF15303A)

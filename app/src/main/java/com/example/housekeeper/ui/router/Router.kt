add67b19d98dd422c6ea6aa32d0e1a16d45e77b0
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 19:38:16 +0300

    feat: Fixed a paddings bug & added functional to displaying a current page

--- app/src/main/java/com/example/housekeeper/ui/router/Router.kt
+++ app/src/main/java/com/example/housekeeper/ui/router/Router.kt
@@ -2,58 +2,122 @@ package com.example.housekeeper.ui.router
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.housekeeper.R
 import com.example.housekeeper.domain.navigation.Screen
 import com.example.housekeeper.ui.theme.MidnightVelvet
 
 @Composable
 fun Router(navController: NavHostController) {
+    var currentScreen by remember { mutableStateOf(Screen.ReminderScreen.route) }
+
     BottomAppBar(
-        modifier = Modifier.fillMaxWidth(),
-        contentColor = MidnightVelvet
+        containerColor = MidnightVelvet
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = { navController.navigate(Screen.ReminderScreen.route) }
+        IconToggleButton(
+            modifier = Modifier.size(50.dp),
+            checked = currentScreen == Screen.ReminderScreen.route,
+            onCheckedChange = {
+                if (!it) return@IconToggleButton
+                currentScreen = Screen.ReminderScreen.route
+                navController.navigate(Screen.ReminderScreen.route)
+            }
         ) {
+            val reminderIcon = if (currentScreen == Screen.ReminderScreen.route) {
+                R.drawable.reminder_active_icon
+            } else {
+                R.drawable.reminder_no_active_icon
+            }
             Image(
-                painter = painterResource(R.drawable.reminder_active_icon),
-                contentDescription = "Reminder active icon"
+                painter = painterResource(reminderIcon),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
             )
         }
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = {}
+        IconToggleButton(
+            modifier = Modifier.size(50.dp),
+            checked = currentScreen == Screen.ReminderScreen.route,
+            onCheckedChange = {
+                if (!it) return@IconToggleButton
+//                currentScreen = Screen.TimerScreen.route
+//                navController.navigate(Screen.TimerScreen.route)
+            }
         ) {
+            val timerIcon = if (currentScreen == Screen.ReminderScreen.route) {
+                R.drawable.timer_active_icon
+            } else {
+                R.drawable.timer_no_active_icon
+            }
             Image(
-                painter = painterResource(R.drawable.timer_no_active_icon),
-                contentDescription = "Reminder active icon"
+                painter = painterResource(timerIcon),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
             )
         }
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = {}
+        IconToggleButton(
+            modifier = Modifier.size(50.dp),
+            checked = currentScreen == Screen.ReminderScreen.route,
+            onCheckedChange = {
+                if (!it) return@IconToggleButton
+//                currentScreen = Screen.CleanerScreen.route
+//                navController.navigate(Screen.CleanerScreen.route)
+            }
         ) {
+            val cleanerIcon = if (currentScreen == Screen.ReminderScreen.route) {
+                R.drawable.cleaner_active_icon
+            } else {
+                R.drawable.cleaner_no_active_icon
+            }
             Image(
-                painter = painterResource(R.drawable.cleaner_no_active_icon),
-                contentDescription = "Reminder active icon"
+                painter = painterResource(cleanerIcon),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
             )
         }
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = {}
+        IconToggleButton(
+            modifier = Modifier.size(50.dp),
+            checked = currentScreen == Screen.ReminderScreen.route,
+            onCheckedChange = {
+                if (!it) return@IconToggleButton
+//                currentScreen = Screen.SettingsScreen.route
+//                navController.navigate(Screen.SettingsScreen.route)
+            }
         ) {
+            val settingsIcon = if (currentScreen == Screen.ReminderScreen.route) {
+                R.drawable.settings_active_icon
+            } else {
+                R.drawable.settings_no_active_icon
+            }
             Image(
-                painter = painterResource(R.drawable.settings_no_active_icon),
-                contentDescription = "Reminder active icon"
+                painter = painterResource(settingsIcon),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
             )
         }
+        Spacer(modifier = Modifier.weight(1f))
     }
+}
+
+
+@Composable
+@Preview
+private fun RouterPreview() {
+    Router(navController = rememberNavController())
 }

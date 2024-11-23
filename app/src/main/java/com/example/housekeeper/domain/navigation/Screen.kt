4991217b15e6bd2d7e8935c10ee6cbf44fdca6f9
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 19:39:25 +0300

    feat: added TimerScreen, CleanerScreen & SettingScreen

--- app/src/main/java/com/example/housekeeper/domain/navigation/Screen.kt
+++ app/src/main/java/com/example/housekeeper/domain/navigation/Screen.kt
@@ -2,5 +2,7 @@ package com.example.housekeeper.domain.navigation
 
 sealed class Screen(val route: String) {
     data object ReminderScreen: Screen(route = "reminder_screen")
+    data object TimerScreen: Screen(route = "timer_screen")
+    data object CleanerScreen: Screen(route = "cleaner_screen")
     data object SettingsScreen: Screen(route = "settings_screen")
 }

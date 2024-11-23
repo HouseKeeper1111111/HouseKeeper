239242d18263465349fd19940936ac54dfabd682
Victoria Zviahintsava <vika.zv04@yandex.ru>
2024-11-23 17:28:21 +0300

    refactoring: Refactored from ReminderScreen.kt to ReminderView.kt

--- app/src/main/java/com/example/housekeeper/ui/pages/ReminderView.kt
+++ app/src/main/java/com/example/housekeeper/ui/pages/ReminderView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +15,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,11 +30,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.housekeeper.R
 import com.example.housekeeper.ui.theme.HouseKeeperTextStyles
+import com.example.housekeeper.ui.theme.MidnightVelvet
 import com.example.housekeeper.ui.theme.PinkChampagne
 import com.example.housekeeper.ui.theme.rememberScreenSize
 
 @Composable
-fun ReminderScreen() {
+fun ReminderView(
+    onNavigateToSettingsScreen: () -> Unit
+) {
     val screenSize = rememberScreenSize()
     val categoriesList = listOf(
         Pair(R.drawable.kitchen_icon, "Kitchen"),
@@ -100,6 +105,14 @@ fun ReminderScreen() {
                     }
                 }
             }
+        },
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                contentColor = MidnightVelvet
+            ) {
+
+            }
         }
     )
 }
@@ -107,6 +120,6 @@ fun ReminderScreen() {
 
 @Composable
 @Preview(device = Devices.PIXEL_7_PRO)
-private fun ReminderScreenPreview() {
-    ReminderScreen()
+private fun ReminderViewPreview() {
+    ReminderView(onNavigateToSettingsScreen = {})
 }
